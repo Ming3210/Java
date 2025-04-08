4a1ba62bc479ac6c16e9c618ffd82a278ef6e40c
unknown <vminh0227@gmail.com>
2025-04-08 12:13:44 +0700

    add

--- Java_practice_04-master/src/jdbc/bussiness/config/ConnectionDB.java
+++ Java_practice_04-master/src/jdbc/bussiness/config/ConnectionDB.java
@@ -10,7 +10,7 @@ public class ConnectionDB {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/StudentManagement";
     private static final String USER_NAME = "root";
-    private static final String PASSWORD = "12345";
+    private static final String PASSWORD = "123456";
 
     public static Connection openConnection() {
         Connection conn = null;
